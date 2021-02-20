1a0b982c486b9537a327dc8e6a081346fb365bf8
Olabode Opeyemi <eopeyemie@gmail.com>
2021-02-20 20:39:29 +0100

    Delete node_modules directory

--- node_modules/ipfs-http-client/src/bitswap/index.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-module.exports = config => ({
-  wantlist: require('./wantlist')(config),
-  wantlistForPeer: require('./wantlist-for-peer')(config),
-  stat: require('./stat')(config),
-  unwant: require('./unwant')(config)
-})
--- node_modules/ipfs-http-client/src/key/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-module.exports = config => ({
-  gen: require('./gen')(config),
-  list: require('./list')(config),
-  rename: require('./rename')(config),
-  rm: require('./rm')(config),
-  import: require('./import')(config)
-})
